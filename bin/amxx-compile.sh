0c437181b5e570c9aa8b8a5302263d2e4daacb1d
Mateusz Derks <mateusz.derks@gmail.com>
2014-08-17 00:55:30 +0200

    Create amxx-compile.sh

---
+++ bin/amxx-compile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+version="1.8.2"
+base_path=`pwd`
+
+if [ $# -eq 0 ]; then
+    echo "No Args";
+else
+    cd amxmodx-$version/addons/amxmodx/scripting && ./amxxpc "$base_path/$1" -o$base_path/$1.amxx
+fi
