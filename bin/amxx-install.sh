7b6a552f04cce1c95f5accd1a0927e940a809ef7
Mateusz Derks <mateusz.derks@gmail.com>
2014-08-17 00:55:12 +0200

    Create amxx-install.sh

---
+++ bin/amxx-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+version="1.8.2"
+base_url="http://y4kstudios.com/sourcemod/mirror/amxmodx-1.8.2-base-linux.tar.gz"
+
+mkdir amxmodx-$version
+cd amxmodx-$version
+
+wget $base_url -O package-base.tar.gz
+tar -zxvf package-base.tar.gz
