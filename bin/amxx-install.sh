817014ffe1bc52ef82b753965ee42ca4cefa7bbb
Mateusz Derks <mateusz.derks@gmail.com>
2014-08-17 01:44:39 +0200

    Update amxx-install.sh

--- bin/amxx-install.sh
+++ bin/amxx-install.sh
@@ -8,4 +8,4 @@ cd amxmodx-$version
 wget $base_url -O package-base.tar.gz
 tar -zxvf package-base.tar.gz
 chmod -R 0777 .
-export PATH=$PATH:$PWD/amxmodx-$version/addonds/amxmodx/scripting
+export PATH=$PATH:$PWD/amxmodx-$version/addons/amxmodx/scripting
