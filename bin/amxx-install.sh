9e372a14e359019018fc42bcb8a9840d5075fe89
Mateusz Derks <mateusz.derks@gmail.com>
2014-08-17 13:20:53 +0200

    Update amxx-install.sh

--- bin/amxx-install.sh
+++ bin/amxx-install.sh
@@ -6,10 +6,10 @@ git checkout tags/amxmodx-1.8.2
 wget https://raw2.github.com/alliedmodders/amxmodx/master/support/checkout-deps.sh
 bash checkout-deps.sh --no-mysql
 
-mkdir build
-cd build
-python ../amxmodx/configure.py --no-mysql
+python amxmodx/configure.py --no-mysql
+
+ambuild build
 
 #Test installation
-cd packages/base/addons/amxmodx/scripting
+cd build/packages/base/addons/amxmodx/scripting
 ./compile.sh
