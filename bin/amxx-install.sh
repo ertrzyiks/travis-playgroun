e2cf4f3d69c52dd62b5273fca1397dae55d063f9
Mateusz Derks <mateusz.derks@gmail.com>
2014-08-20 22:28:28 +0200

    Update amxx-install.sh

--- bin/amxx-install.sh
+++ bin/amxx-install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-wget http://sourcemod.gameconnect.net/files/amxmodx-1.8.2-base-linux.tar.gz
-tar -xvf amxmodx-1.8.2-base-linux.tar.gz
-rm -f amxmodx-1.8.2-base-linux.tar.gz
+git clone https://github.com/alliedmodders/amxmodx.git
+git fetch
+git checkout tags/amxmodx-1.8.2
+
+wget https://raw2.github.com/alliedmodders/amxmodx/master/support/checkout-deps.sh
+bash checkout-deps.sh --no-mysql
+
+mkdir build
+cd build
+python ../amxmodx/configure.py --no-mysql
+cd ..
+
+ambuild build
